49218af5a8a9437310c15d2f6d3d87b9f41f5aef
fvlgnn <fvlgnn@gmail.com>
2021-09-22 12:45:06 +0200

    prima versione alpha

--- README.md
+++ README.md
@@ -1,2 +1,144 @@
 # SPID Test Automation
-Automatizzazione dei test AgID per l'integrazione di SPID. Python, Pip, Selenium, Chrome, Docker.
+Automatizzazione dei test AgID del simulatore Identity Provider _IdP_ [SPID SAML Check](https://github.com/italia/spid-saml-check) per l'integrazione di SPID.
+
+
+## Descrizione
+
+Questo sistema automatizzato aiuta l'utente a verificare il centinaio di test che AgID richiede affinché un Service Provider _SP_ possa integrare sul proprio ambiente la login tramite SPID.
+
+Il sistema è sviluppato tramite Python ed utilizza Selenium che interagisce su un browser (attualmente Chrome). Il sistema può essere eseguito direttamente da Python oppure eseguito in un container Docker/Docker compose, o lanciato da un orchestratore (Kubernates/Openshift) utilizzando una versione Jenkins Slave per eseguire il pull dell'immagine Docker e avviare la procedura automatizzata dei test AgID.
+
+I parametri personalizzabili del sistema possono essere passati tramite argomenti _args_ o tramite variabili d'ambiente _env_.
+
+
+----
+
+
+## Requisiti d'ambiente
+
+- IdP _SPID SAML Check_ installato su ambiente (vedi documentazione ufficiale _Developers Italia_ https://github.com/italia/spid-saml-check)
+- WebApp Service Provider _SP_ con login SPID e con abilitato come Identity Provider _IdP_ il validatore _SPID SAML Check_
+- Python 3
+- Pip
+- Selenium
+- Chrome
+- Docker (opzionale)
+- Kubernates (opzionale)
+- Openshift (opzionale)
+- Jenkins (opzionale)
+
+
+----
+
+
+## Guida all'uso
+
+**Nota Bene!** Sicuramente all'interno dello script principale `main.py`, alcuni tags HTML che utilizza Selenium per catturare le sezioni della WebApp personale, in modo tale da simulare le azioni di login, andranno modificati, mentre quelli inerenti al validatore _SPID SAML Check_ sono configurati in modo tale da non richiedere ulteriori modifiche.
+
+
+### Parametri
+
+Paramenti delle variabili per i test.
+
+#### Argomenti
+
+Eseguire lo script passando o meno i seguenti argomenti.
+
+    -h, --help                                  show this help message and exit
+    --first FIRST                               Test di partenza (default: 1)
+    --last LAST                                 Test di arrivo (default: 111)
+    --exclude EXCLUDE [EXCLUDE ...]             Test da escludere (es. 5 6 7). Di default i test che AgId non verifica (default: [5, 6, 7, 50, 101, 102])
+    --custom-test CUSTOM_TEST [CUSTOM_TEST ...] Esegue solo i test nella lista (es. 32 94 95 96 111) (default: None)
+    --url URL                                   URL di partenza (default: https://tua-webapp-con-login-spid.tld)
+    --meta META                                 URL del metadata del SP (default: https://tuo-service-provider-dove-leggere-il-metadata-spid.tld/spid-sp-metadata)
+    --target TARGET                             HTML <title> della pagina di destinazione (default: TestSpidWebApp)
+    --custom-user CUSTOM_USER                   True modifica CF e Email nella Response vedi --cf e --email, False come da test.json (default: false)
+    --fiscal-number FISCAL_NUMBER               Codice fiscale con prefisso TINIT- dell'utente di test (default: TINIT-GDASDV00A01H501J)
+    --email EMAIL                               Email dell'utente di test (default: spid.tech@agid.gov.it)
+    --level LEVEL                               Livello SPID. Usa 1, 2 o 3 (default: 1)
+    --delay DELAY                               Tempo tra un'azione e l'altra (default: 0.4)
+    --logout LOGOUT                             True, forza il logout se la sessione è attiva (default: true)
+    --container CONTAINER                       True se eseguito da dentro un container, False visualizza il browser all'utente per debug (default: true)
+    --logs LOGS                                 False per live usa stdout, True per debug scrive logs su FS (default: false)
+
+In particolare `--container` usa chrome in modalità _headless_, _no-sandbox_, disabilitando la gpu e altre cose che servono per far girare chrome dentro un container. 
+
+**Nota Bene!** Per prime istallazioni e/o debug è consigliabile eseguire lo script direttamente tramite python e impostando lo switch `--container false`. NON USARE l'argomento _container false_ all'interno di un container o di un orchestratore, non potrà funzionare. Per l'ambiente di sviluppo è consigliabile usare il _virtual environments venv_ di python, vedi https://docs.python.org/3/library/venv.html.
+
+
+#### System Environment
+
+TODO
+
+
+#### Dot-Env (Variabili da file `.env`)
+
+TODO
+
+
+
+### Esempi pratici
+
+Lista di comandi da eseguire in base al sistema in uso con esempi pratici e spiegazioni.
+
+#### Python
+
+Direttamente tramite python.
+
+- `python3 main.py` 
+
+Esegue i test come da parametri di default. **Nota Bene!** Se non sono state apportate modifiche al sorgente, quantomeno ai parametri _url_ e _metadata_, sicuramente non può funzionare :|
+
+- `python3 main.py --first 44 --last 60 --delay 1.5 --target 'Pagina di Test' --custom-user true`
+
+Il comando sopra lancia l'automatizzazione dei test dal 44 al 60, con un delay tra un operazione e l'altra di un secondo e mezzo, dove _Pagina di Test_ è il titolo HTML compreso nel tag _<title></title>_ della pagina di arrivo dopo aver effettuato l'accesso e sulla _Response_ del validatore viene modificato _fiscalNumber_ (--fiscal-number) e _email_ (--email) come da parametri di default.
+
+- `python3 main.py --url https://localhost:8080 --meta https://localhost:8081/metadata.xml --container false --logs true --custom-test 1 94 95 96 111`
+
+Il comando sopra lancia l'automatizzazione dei test in localhost (verificare che SPID SAML Check stia girando in locale), prelevando il metadata da una web app in locale che gira sulla porta 8081, visualizzando il browser, scrivendo i logs - all'interno della cartella _logs_ e verificando solo i test _1, 94, 95, 96, 111_.
+
+
+#### Docker
+
+All'interno di un container Docker.
+
+- `docker build -t spid-test-automation` o con Dockerfile personalizzato es. `docker build -f Dockerfile-python-env -t spid-test-automation`
+- `docker run -it spid-test-automation`
+
+Per vedere l'output dal container.
+
+Immagine sul registry docker
+
+Per altre informazioni si consiglia di consultare la documentazione ufficiale di Docker
+
+
+#### Jenkins Slave su Openshift / Kubernates 
+
+- visionare\modificare `Dockerfile-openshift-jenkins-slave`
+- eseguire la build `docker build -f Dockerfile-openshift-jenkins-slave -t spid-test-automation`
+- assegnare un tag all'immagine `docker image tag spid-test-automation nome-del-tuo-registry/spid-test-automation:latest`
+- caricare l'immagine su un docker registry `docker push nome-del-tuo-registry/spid-test-automation:latest`
+
+
+- configurare l'orchestratore per utilizzare l'immagine
+- configurare i parametri della pipline di Jenkins esempio di seguito 
+
+```
+pipeline {
+    agent any
+    stages {
+        stage("SPID Test Automation") {
+            agent {
+                label 'spid-test-automation'
+            }   
+            steps {
+                script {
+                    sh "python3 /main.py --url ${env.WEBAPP_TEST_PAGE_URL} --meta ${env.WEBAPP_TEST_METADATA_URL} --target ${env.WEBAPP_TEST_PAGE_TITLE}"
+                }
+            }
+        }
+    }
+}
+```
+
+
---
+++ main.py
@@ -0,0 +1,339 @@
+#!/usr/bin/env python3
+
+# -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
+#
+# Copyright (c) 2021 Gianni Favilli @fvlgnn
+# MIT License.
+#
+# Description: SPID Test Automation
+#
+# Date: 14/06/2021
+#
+# Update: 22/09/2021
+#
+# coding: utf-8
+#
+# -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
+
+import sys, urllib3, os, chromedriver_binary
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+
+from time import sleep
+from datetime import datetime
+
+urllib3.disable_warnings()
+
+yep = ['true', '1', 't', 'y', 'yes', 'yep', 'ok', 'si', 'sì', 's']
+
+#NOTE CONFIG WITH ARGS ----
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--first', type=int, default=1, help="Test di partenza", required=False)
+parser.add_argument('--last', type=int, default=111, help="Test di arrivo", required=False)
+parser.add_argument('--exclude', type=int, default=[5,6,7,50,101,102], nargs='+', help="Test da escludere (es. 5 6 7). Di default i test che AgID non verifica", required=False)
+parser.add_argument('--custom-test', type=int, nargs='+', help="Esegue solo i test nella lista (es. 1 32 94 95 96 111)", required=False)
+
+parser.add_argument('--url', type=str, default='https://tua-webapp-con-login-spid.tld', help="URL di partenza", required=False)
+parser.add_argument('--meta', type=str, default='https://tuo-service-provider-dove-leggere-il-metadata-spid.tld/spid-sp-metadata', help="URL del metadata del SP", required=False)
+parser.add_argument('--target', type=str, default='TestSpidWebApp', help="HTML <title> della pagina di destinazione", required=False)
+
+parser.add_argument('--custom-user', type=str, default='false', help="True modifica CF e Email nella Response come --cf e --email, False come da test.json", required=False)
+parser.add_argument('--fiscal-number', type=str, default='TINIT-GDASDV00A01H501J', help="Codice fiscale con prefisso TINIT- dell'utente di test", required=False)
+parser.add_argument('--email', type=str, default='spid.tech@agid.gov.it', help="Email dell'utente di test", required=False)
+parser.add_argument('--level', type=int, default=1, help="Livello SPID. Usa 1, 2 o 3", required=False)
+
+parser.add_argument('--delay', type=float, default=0.4, help="Tempo tra un'azione e l'altra", required=False)
+parser.add_argument('--logout', type=str, default='true', help="True, forza il logout se la sessione è attiva", required=False)
+parser.add_argument('--container', type=str, default='true', help="True se eseguito da dentro un container, False visualizza il browser all'utente per debug", required=False)
+parser.add_argument('--logs', type=str, default='false', help="False per live usa stdout, True per debug scrive logs su FS", required=False)
+
+args = parser.parse_args()
+
+write_logs = args.logs.lower() in yep
+is_container = args.container.lower() in yep
+is_custom_user  = args.custom_user.lower() in yep
+logout  = args.logout.lower() in yep
+
+test_first = args.first
+test_last = args.last
+test_exclude = args.exclude
+test_custom = args.custom_test
+
+siteurl = args.url
+metadata = args.meta
+target_page_title = args.target
+
+spid_level = args.level
+
+fiscal_number = args.fiscal_number
+email = args.email
+
+delay = args.delay
+
+#  ----
+
+# TODO CONFIG WITH DOT-ENV ----
+
+# siteurl = os.environ('URL')
+# ...
+# delay = os.getenv('DELAY')
+
+#  ----
+
+# TODO CONFIG WITH SYSTEM ENVIRONMENT ----
+
+# from dotenv import load_dotenv
+# load_dotenv()
+# siteurl = os.getenv('URL')
+# ...
+# delay = os.getenv('DELAY')
+
+#  ----
+
+# LOGS or STDOUT
+
+if write_logs:
+    import logging, logging.handlers
+    scriptPath = os.path.dirname(__file__)
+    logDir = os.path.join(scriptPath, 'logs')
+    logsFile = os.path.join(logDir, 'reports.log')
+    formatter = logging.Formatter('%(message)s')
+    handler = logging.handlers.TimedRotatingFileHandler(logsFile, when='midnight', interval=1, backupCount=8)
+    handler.suffix = "%y%m%d" + ".log"
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+def logme(message, level = 'info'):
+    if write_logs:
+        log = str(level.upper()) + ' - ' + str(message)
+        logger.info(log)
+    else:
+        log = str(datetime.now()) + ': ' + str(level.upper()) + ' - ' + str(message)        
+    print(log)
+    # if is_container:
+    #     # sys.stdout = open(1, 'w', encoding='utf-8', closed=False)
+    #     sys.stdout.flush()
+
+# ----
+
+# NOTE MAIN ----
+
+def main():       
+
+    try:
+
+        next = True
+
+        if test_custom is None:
+            tests = [i for i in range(test_first, test_last + 1) if i not in test_exclude]
+        else:
+            tests = test_custom
+
+        if is_container:
+            logme("Integration Tests Configurations\n")
+            logme(f"tests: {tests}")
+            logme(f"siteurl: {siteurl}")
+            logme(f"metadata: {metadata}")
+            logme(f"target_page_title: {target_page_title}")
+            logme(f"spid_level: {spid_level}")
+            logme("End Show Configurations\n")
+
+        logme(f"TEST Started ({datetime.now()}) **** ---- **** ---- **** ---- **** ----\n")           
+
+        for test in tests:
+
+            if next:
+
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.add_argument("incognito")
+                chrome_options.add_argument("disable-web-security")
+                chrome_options.add_argument("start-maximized")
+                chrome_options.add_argument("ignore-certificate-errors-spki-list")
+                chrome_options.add_argument("ignore-certificate-errors")
+                    
+                if is_container:
+                                  
+                    chrome_options.add_argument("disable-gpu")
+                    chrome_options.add_argument("no-sandbox")
+                    chrome_options.add_argument("disable-dev-shm-usage")
+                    chrome_options.add_argument("headless")
+                    chrome_options.add_argument("window-size=1920,1080")
+
+                driver = webdriver.Chrome(options=chrome_options)
+
+                try:
+
+                    driver.get(siteurl)
+                    sleep(delay)
+
+                    #NOTE PERSONALIZZAZIONE. Da modificare in base alla propria webapp
+                    sleep(delay)
+                    driver.find_element_by_xpath("//html").click()
+                    sleep(delay)
+                    driver.find_element_by_xpath('//a[@href="#spid"]').click()
+                    sleep(delay * 2)
+                    driver.find_element_by_partial_link_text("Entra con SPID").click()
+                    sleep(delay)
+                    driver.find_element_by_id("zocial-spid-tester").click()
+                    sleep(delay)
+                    #NOTE fine PERSONALIZZAZIONE.
+
+                    try:
+                        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Username"]')))
+                    except TimeoutException:
+                       logme(f"TEST {test} - Timeout SPID Validator IdP", "error")
+                       next = False
+
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@placeholder="Username"]').click()
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("validator")
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@placeholder="Password"]').click()
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("validator")
+                    sleep(delay)
+                    driver.find_element_by_xpath("//button[text()='Login']").click()
+                    sleep(delay)
+
+                    #NOTE Aggiornamento del Metadata SP
+                    try:
+                        driver.find_element_by_id("Worksave").is_displayed()                
+                        if test == 1:
+                            driver.find_element_by_class_name("worksave-img-new").click()
+                            sleep(delay * 2)
+                            driver.switch_to.alert.accept()
+                            sleep(delay * 3)
+                            driver.find_element_by_xpath("//a[text()='Metadata SP']").click()
+                            sleep(delay * 3)
+                            driver.find_element_by_partial_link_text("Download").click()
+                            sleep(delay)
+                            driver.find_element_by_xpath('//input[@class="metadata"]').send_keys(metadata)
+                            sleep(delay)
+                            driver.find_element_by_xpath("//button[text()='Download']").click()
+                        else:
+                            driver.find_element_by_class_name("worksave-img-continue").click() 
+                    except NoSuchElementException:
+                        sleep(delay * 3)
+                        driver.find_element_by_xpath("//a[text()='Metadata SP']").click()
+                        sleep(delay * 3)
+                        driver.find_element_by_partial_link_text("Download").click()
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@class="metadata"]').send_keys(metadata)
+                        sleep(delay)
+                        driver.find_element_by_xpath("//button[text()='Download']").click()
+                        pass
+
+                    sleep(delay * 3)
+                    driver.find_element_by_xpath("//a[text()='Response']").click()
+                    sleep(delay * 3)
+                    driver.find_element_by_partial_link_text("Check Response").click()
+                    sleep(delay)
+
+                    sleep(delay)
+                    driver.find_element_by_id("react-select-2--value").click()
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@id="response-select"]').send_keys(f"{test}.")
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@id="response-select"]').send_keys(Keys.TAB)
+                    sleep(delay * 2)
+                    expected_result = driver.find_element_by_class_name("test-description").text
+                    key_result = expected_result.split(" ")[-1].lower()
+                    sleep(delay)
+
+                    #TODO aggiungere azione per premre il pulsante di test eseguito
+
+                    if is_custom_user:
+
+                        driver.execute_script("window.scrollTo(0, 1800)")
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="fiscalNumber"]').click()
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="fiscalNumber"]').clear()
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="fiscalNumber"]').send_keys(fiscal_number)
+                        sleep(delay * 2)
+
+                        driver.execute_script("window.scrollTo(0, 1800)")
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="email"]').click()
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="email"]').clear()
+                        sleep(delay)
+                        driver.find_element_by_xpath('//input[@placeholder="email"]').send_keys(email)
+                        sleep(delay * 2)
+
+                        driver.execute_script("window.scrollTo(0, 0)")
+                        sleep(delay)
+
+                    sleep(delay)
+                    driver.find_element_by_xpath('//input[@type="submit"]').click()
+                    sleep(delay)
+
+                    sleep(delay * 4)                
+
+                    if len(driver.window_handles) > 1:
+                        driver.switch_to.window(driver.window_handles[1])
+                    sleep(delay)
+                    title_page = driver.title
+                    
+                    try:
+                        spid_log_msg = ""
+                        if test in range(94,97):
+                            spid_log_msg = f"\nSPID Level set for test: {spid_level}"
+                        if key_result == "ok" or key_result == "request.":
+                            if title_page == target_page_title or title_page == "Error - Utente Non Autorizzato":
+                                logme(f"TEST_{test:03}\nTest description: {expected_result}{spid_log_msg}\nResult: Web App (Title page: {title_page})\n", "passed")
+                                #NOTE PERSONALIZZAZIONE. Da modificare in base alla propria webapp
+                                if title_page == target_page_title and logout:
+                                    try:
+                                        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//button[@id="dropdownMenuButton"]')))
+                                        sleep(delay)
+                                        driver.find_element_by_xpath('//button[@id="dropdownMenuButton"]').click()
+                                        sleep(delay)
+                                        driver.find_element_by_xpath("//a[text()='Logout']").click()
+                                        sleep(delay * 2)
+                                    except TimeoutException:
+                                        pass
+                                #NOTE fine PERSONALIZZAZIONE.
+                            else:
+                                logme(f"TEST_{test:03}\nTest description: {expected_result}{spid_log_msg}\nTitle page: {title_page}\n", 'not passed')
+                        else:
+                            error_message = driver.find_element_by_id("kc-error-message").text.splitlines()[0]
+                            logme(f"TEST_{test:03}\nTest description: {expected_result}{spid_log_msg}\nResult description: {error_message}\n", "passed")
+                    except NoSuchElementException:
+                        print("sono qui")
+                        logme(f"TEST_{test:03}\nTest description: {expected_result}{spid_log_msg}\n{spid_log_msg}Incorrect expected page (Title page: {title_page})\n", "not passed")
+
+                except NoSuchElementException as err:
+                    logme("TEST " + str(test) + " " + str(err).splitlines()[0], "error")
+                    next = False
+            
+                driver.quit()
+
+    except Exception as e:
+        logme(e, 'error')
+    finally:
+        # driver.quit()
+        logme(f"TEST Finished ({datetime.now()}) **** ---- **** ---- **** ---- **** ----\n\n")
+        sleep(delay * 2)
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
